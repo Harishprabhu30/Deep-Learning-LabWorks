19042ffa6ec24c852dce0898497be1cff63d9573
Harish Prabhu <111897267+Harishprabhu30@users.noreply.github.com>
2025-11-22 02:21:02 +0200

    Added - scripts to segregated cats, dogs into separate folders, validation folder created

---
+++ Lab_3/train_val_split.py
@@ -0,0 +1,19 @@
+import os, shutil, random
+
+SOURCE = "/Users/harishprabhu/desktop/subjects/sem_3/deep_learning/lab_3/data/train"
+VAL = "/Users/harishprabhu/desktop/subjects/sem_3/deep_learning/lab_3/data/val"
+SPLIT = 0.2  # 20%
+
+os.makedirs(f"{VAL}/cats", exist_ok=True)
+os.makedirs(f"{VAL}/dogs", exist_ok=True)
+
+for cls in ["cats", "dogs"]:
+    files = os.listdir(f"{SOURCE}/{cls}")
+    random.shuffle(files)
+    val_count = int(len(files) * SPLIT)
+    val_files = files[:val_count]
+
+    for f in val_files:
+        shutil.move(f"{SOURCE}/{cls}/{f}", f"{VAL}/{cls}/{f}")
+
+print("Validation split created.")
